5a07d53b07f9b8815e4dbea5622cdaac8161b642
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-09-04 22:58:52 -0700

    Add google analytics

--- wp-content/themes/sandbox/footer.php
+++ wp-content/themes/sandbox/footer.php
@@ -1,4 +1,3 @@
-
 	<div id="footer">
 		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e('WordPress', 'sandbox'); ?>" rel="generator"><?php _e('WordPress', 'sandbox'); ?></a></span>
 		<span class="meta-sep">|</span>
@@ -8,6 +7,15 @@
 </div><!-- #wrapper .hfeed -->
 
 <?php wp_footer() ?>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-1191976-4");
+pageTracker._trackPageview();
+</script>
 
 </body>
 </html>
